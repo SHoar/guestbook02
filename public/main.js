02c81a0cb570b29e61dd254c37175a86b3c3cd2f
Sean Hoar <sean.hoar@gmail.com>
2016-11-07 14:54:31 -0500

    Initialize photoslide

--- public/main.js
+++ public/main.js
@@ -1,6 +1,27 @@
-<ul id="comment-list">
-  {{#each comments}}
-  <li class="comment-list-item">{{this}}</li>
-  {{/each}}
-</ul>
-   
+/* Time-transition photoslide */
+var gallery = document.querySelector('.js-gallery')
+var galleryItems = gallery.querySelectorAll('.js-gallery-item')
+
+var slideCount = galleryItems.length
+var slideWidth = galleryItems[0].getBoundingClientRect().width
+
+// var slideInterval = setInterval(transitionSlide, 5000)
+var currentSlide = 1
+
+var sliderNav = document.querySelector('.js-nav-list')
+sliderNav.addEventListener('click', transitionSlide)
+
+function transitionSlide( e ) {
+  e.preventDefault()
+
+  var currentDirection = e.target.dataset.direction
+  if (currentDirection === 'left' && currentSlide === 1) return
+  if (currentDirection === 'right' && currentSlide === slideCount) return
+  if ( currentDirection === 'left' ) {
+    currentSlide--
+  } else {
+    currentSlide++
+  }
+  gallery.style.transform = "translateX(-" + slideWidth * (currentSlide - 1) + "px)"
+  console.log( currentSlide )
+}
